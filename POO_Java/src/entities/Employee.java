5d824451c742e611162490c896189a563eda838b
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2025-11-27 23:11:19 -0300

    Exercicio de Matriz

--- POO_Java/src/entities/Employee.java
+++ POO_Java/src/entities/Employee.java
@@ -14,26 +14,15 @@ public class Employee {
 	public Integer getId() {
 		return id;
 	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public Double getSalary() {
 		return salary;
 	}
 
-	public void setSalary(Double salary) {
-		this.salary = salary;
-	}
 	
 	public void increaseSalary(double percentage) {
 		double raise = salary * (percentage / 100);
